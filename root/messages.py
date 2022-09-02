5044aa9b1e4b9314f531fbdc2a631bfde0cfb59b
Instantig <112691843+Instantig@users.noreply.github.com>
2022-09-03 03:29:51 +0530

    Update messages.py

--- root/messages.py
+++ root/messages.py
@@ -1,11 +1,11 @@
 class Translation(object):
-    START_TEXT = """**I'm a Rename and Convert Bot
-Just send me any media to change file name.
-Use /help command for more details **
+    START_TEXT = """**I'm a Smart Rename and Convert Bot
+Just send me any media to change file name
+Use /help command for more details **\n**Powered By @the_glitchs**
 """
-    HELP_USER = """**>>Send File/Video\n>>Select desired Option\n>>And Done wait for it to process files**"""
-    DOWNLOAD_MSG = "**Downloading **⏬"
+    HELP_USER = """**>>Send File/Video\n>>Select desired Option\n>>And Done wait for it to process files**\n>>**Need More Help Contact Support @hddubhub4uhelp | @the_glitchs**"""
+    DOWNLOAD_MSG = "**Downloading **🔁"
     DOWNLOAD_FAIL_MSG = "**Failed to Download File**❎"
-    UPLOAD_MSG = "**Uploading** ⏫"
-    UPLOAD_FAIL_MSG = "**Failed to Upload File**❎"
-    UPLOAD_DONE_MSG = "**Uploaded Successfully 💡"
+    UPLOAD_MSG = "**Uploading** ⏬"
+    UPLOAD_FAIL_MSG = "**Failed to Upload File**❌"
+    UPLOAD_DONE_MSG = "**Uploaded Successfully ✔\n**Try Other Bots | @robo_glitch** 💡"
